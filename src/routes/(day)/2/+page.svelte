cea2e45e0997e6ac90c334a33f7c24bc1c4b3a61
rosancoderian <rosancoderian@gmail.com>
2024-01-01 21:02:15 +0700

    feat: update day2

--- src/routes/(day)/2/+page.svelte
+++ src/routes/(day)/2/+page.svelte
@@ -1,3 +1,28 @@
-<div class="mx-auto flex h-[924px] w-[1024px] flex-col justify-between">
+<script>
+	import { Button } from "$lib/components/ui/button"
+	import { Progress } from "$lib/components/ui/progress"
+	import { Cookie } from "phosphor-svelte"
 
+	let cookies = $state(5)
+	let eaten = $state(0)
+</script>
+
+<div class="mx-auto flex h-[924px] w-[1024px] flex-col gap-2">
+	<div class="flex items-center gap-2">
+		<Button on:click={() => cookies++}>Add a cookie</Button>
+		<Button on:click={() => (eaten < cookies ? eaten++ : null)}>Eat a cookie</Button>
+		<Button on:click={() => (eaten = 0)}>Reset</Button>
+	</div>
+	<div class="flex gap-2">
+		{#each { length: cookies } as _, i}
+			<Cookie size={42} opacity={i + 1 > cookies - eaten ? 0.5 : 1} />
+		{/each}
+	</div>
+	<Progress value={eaten} max={cookies} class="w-full" />
+	<div>
+		Cookies: {cookies}
+	</div>
+	<div>
+		Eaten: {eaten}
+	</div>
 </div>
