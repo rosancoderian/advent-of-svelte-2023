355170a1e66f18b19f540768dbd6d50dcd0e15ff
rosancoderian <rosancoderian@gmail.com>
2024-01-08 14:11:03 +0700

    feat: update day4

--- src/routes/(day)/4/+page.svelte
+++ src/routes/(day)/4/+page.svelte
@@ -1,33 +1,33 @@
 <script lang="ts">
 	import { Heartbeat } from "phosphor-svelte"
 	import { onMount } from "svelte"
-  import { page } from '$app/stores'
+	import { page } from "$app/stores"
 	import { tweened } from "svelte/motion"
-	import { cubicOut } from 'svelte/easing';
+	import { cubicOut } from "svelte/easing"
 
 	let heartRate = $state($page.data.heartRate)
 
-  let heartSize = tweened(1, {
+	let heartSize = tweened(1, {
 		duration: 500,
 		easing: cubicOut
 	})
 
 	onMount(() => {
 		const interval = setInterval(async () => {
-      $heartSize += 0.1
-      heartRate = await fetchHartrate()
-      $heartSize -= 0.1
-    }, 1000)
-    return () => {
-      clearInterval(interval)
-    }
+			$heartSize += 0.1
+			heartRate = await fetchHartrate()
+			$heartSize -= 0.1
+		}, 1000)
+		return () => {
+			clearInterval(interval)
+		}
 	})
 
-  function fetchHartrate() {
-    return fetch("https://advent.sveltesociety.dev/data/2023/day-four.json")
+	function fetchHartrate() {
+		return fetch("https://advent.sveltesociety.dev/data/2023/day-four.json")
 			.then((res) => res.json())
 			.then((data) => data.heartRate)
-  }
+	}
 </script>
 
 <div class="text-center">
--- src/routes/(day)/4/+page.ts
+++ src/routes/(day)/4/+page.ts
@@ -1,6 +1,6 @@
-import type { PageServerLoad } from "./$types"
+import type { PageLoad } from "./$types"
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params }) => {
 	return {
 		heartRate: await fetch("https://advent.sveltesociety.dev/data/2023/day-four.json")
 			.then((res) => res.json())
